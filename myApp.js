fd6a5ec98a6ce28d99abde662de569a341797269
unknown <nodus8tollens>
2023-05-06 18:11:45 +0200

    first commit

--- myApp.js
+++ myApp.js
@@ -1,7 +1,21 @@
 require('dotenv').config();
-
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+  console.log("DB CONNECTION SUCCESSFUL");
+});
 
 let Person;
+
+const personSchema = new mongoose.Schema({
+  name:{
+    type: String,
+    required: true,
+  },
+  age: Number,
+  favoriteFoods: [String]
+});
+
+Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
